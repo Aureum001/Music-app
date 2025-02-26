9bc937075af319698bef8e91f736cc32e11fba76
Aureum001 <antonioayr.94@gmail.com>
2025-02-26 19:34:20 +0100

    Navbar on home diplay

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
+import DisplayHome from "./components/DisplayHome";
+import Display from "./components/Display";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar></Sidebar>
+        <Display></Display>
       </div>
       <Player />
     </div>
